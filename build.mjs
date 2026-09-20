0dc3999f65588c454b8f08b7fe046b21b41d8dab
Manuel Leonhardt <me@skycaptain.dev>
2026-09-20 21:39:45 +0200

    build: bundle extension with esbuild

---
+++ build.mjs
@@ -0,0 +1,58 @@
+// Skycaptain: VSCode "Open in Gitfox" Extension
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+import { context } from "esbuild";
+
+const isProduction = process.argv.includes("--production");
+const isWatch = process.argv.includes("--watch");
+
+/**
+ * @type {import('esbuild').Plugin}
+ */
+const esbuildProblemMatcherPlugin = {
+  name: "esbuild-problem-matcher",
+
+  setup(build) {
+    build.onStart(() => {
+      console.log("[watch] build started");
+    });
+    build.onEnd((result) => {
+      result.errors.forEach(({ text, location }) => {
+        console.error(`✘ [ERROR] ${text}`);
+        console.error(`    ${location.file}:${location.line}:${location.column}:`);
+      });
+      console.log("[watch] build finished");
+    });
+  },
+};
+
+async function main() {
+  const ctx = await context({
+    entryPoints: ["src/extension.ts"],
+    bundle: true,
+    format: "cjs",
+    minify: isProduction,
+    sourcemap: !isProduction,
+    sourcesContent: false,
+    platform: "node",
+    outfile: "dist/extension.js",
+    external: ["vscode"],
+    logLevel: "silent",
+    plugins: [
+      /* add to the end of plugins array */
+      esbuildProblemMatcherPlugin,
+    ],
+  });
+  if (isWatch) {
+    await ctx.watch();
+  } else {
+    await ctx.rebuild();
+    await ctx.dispose();
+  }
+}
+
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
